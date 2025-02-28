c793031095ae31b4dae02ed6ddd85d7f47b80867
cgaleam <cargalmag@alum.us.es>
2025-02-28 20:17:58 +0100

    fix: entity aircraft

--- src/main/java/acme/entities/aircraft/Aircraft.java
+++ src/main/java/acme/entities/aircraft/Aircraft.java
@@ -1,6 +1,7 @@
 
 package acme.entities.aircraft;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -8,9 +9,8 @@ import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
-import org.checkerframework.common.aliasing.qual.Unique;
-
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
@@ -28,28 +28,33 @@ public class Aircraft extends AbstractEntity {
 	@Mandatory
 	@ValidString(max = 50)
 	@Max(50)
+	@Automapped
 	private String				model;
 
 	@Mandatory
 	@ValidString(max = 50)
-	@Unique
+	@Column(unique = true)
 	private String				registration_number;
 
 	@Mandatory
 	@ValidNumber(min = 1, fraction = 0)
 	@Min(1)
+	@Automapped
 	private Integer				capacity;
 
 	@Mandatory
 	@ValidNumber(min = 2000, max = 50000)
+	@Automapped
 	private Double				cargoWeight;
 
 	@Mandatory
 	@Valid
 	@Enumerated(EnumType.STRING)
+	@Automapped
 	private Status				status;
 
 	@Optional
 	@ValidString(max = 250)
+	@Automapped
 	String						details;
 }
