ba3d43edabf03aab5fa68256f1a576834000a32f
Miguel3114 <my.garciaazuara@gmail.com>
2025-03-08 18:49:56 +0100

    feat: first version of Passenger

---
+++ src/main/java/acme/realms/Passenger.java
@@ -0,0 +1,55 @@
+
+package acme.realms;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Passenger extends AbstractRole {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(min = 1, max = 255)
+	@Automapped
+	String						name;
+
+	@Mandatory
+	@ValidEmail()
+	@ValidString(min = 1, max = 255)
+	@Automapped
+	String						email;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z0-9]{6,9}$")
+	@Automapped
+	String						passportNumber;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Automapped
+	Date						dateOfBirth;
+
+	@Optional
+	@ValidString(max = 50)
+	@Automapped
+	String						specialNeeds;
+
+}
