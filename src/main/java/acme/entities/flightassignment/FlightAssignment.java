edd39f67d6b40a82dc13f9991a607e97fa1144b2
cgaleam <cargalmag@alum.us.es>
2025-03-09 14:27:09 +0100

    update: FlightAssignment updateMoment

--- src/main/java/acme/entities/flightassignment/FlightAssignment.java
+++ src/main/java/acme/entities/flightassignment/FlightAssignment.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -37,6 +39,7 @@ public class FlightAssignment extends AbstractEntity {
 	@Mandatory
 	@ValidMoment(min = "2000/01/01  00:00:00", past = true)
 	@Automapped
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date				lastUpdateMoment;
 
 	@Mandatory
