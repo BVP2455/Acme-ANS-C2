5a3af7ee317f79048d9984735d0079c4f1b05a1c
jlcegri <jcegrimarcos@gmail.com>
2025-03-09 21:09:33 +0100

    feat: Entity Flight created
    Mandatory and optional atributes

---
+++ src/main/java/acme/entities/flights/Flight.java
@@ -0,0 +1,55 @@
+
+package acme.entities.flights;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Flight extends AbstractEntity {
+
+	// Serialisation version ----------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Mandatory atributes ------------------------------
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				tag;
+
+	@Mandatory
+	@Automapped
+	private Boolean				selfTransfer;
+
+	@Mandatory
+	@ValidMoney(min = 0.0)
+	@Automapped
+	private Money				cost;
+
+	// Optional atributes -------------------------------
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String				description;
+
+	// Derivated atributes ------------------------------
+
+
+	private Date getScheduledDeparture() {
+
+	}
+
+}
