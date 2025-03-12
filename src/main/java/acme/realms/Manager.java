2d2fda013a67589b9e142f19190409cf8e386242
jlcegri <jcegrimarcos@gmail.com>
2025-03-12 18:23:20 +0100

    feat(Manager): added role Manager
    Added mandatory, optional atributes and custom validations

---
+++ src/main/java/acme/realms/Manager.java
@@ -0,0 +1,55 @@
+
+package acme.realms;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidManager;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@ValidManager
+public class Manager extends AbstractRole {
+
+	// Serialisation version ----------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Mandatory atributes ------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$", message = "Invalid identifier number, please follow pattern")
+	@Column(unique = true)
+	private String				identifier;
+
+	@Mandatory
+	@ValidNumber
+	@Automapped
+	private Integer				yearsExperience;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				birthDate;
+
+	@Optional
+	@ValidUrl
+	@Automapped
+	private String				picture;
+
+}
