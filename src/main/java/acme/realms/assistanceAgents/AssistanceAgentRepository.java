3659992e1a9c569023d122b652afc5b1053c6930
BVP2455 <javier4martosromero@gmail.com>
2025-03-25 16:01:14 +0100

    feat(AssistanceAgent): añadido unicidad

---
+++ src/main/java/acme/realms/assistanceAgents/AssistanceAgentRepository.java
@@ -0,0 +1,14 @@
+
+package acme.realms.assistanceAgents;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+
+@Repository
+public interface AssistanceAgentRepository extends AbstractRepository {
+
+	@Query("SELECT a FROM AssistanceAgent a WHERE a.employeeCode = :employeeCode")
+	AssistanceAgent findByEmployeeCode(String employeeCode);
+}
