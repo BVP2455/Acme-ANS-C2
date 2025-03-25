94ece32b57f74679e664829c9e2f4cff9026d451
jlcegri <jcegrimarcos@gmail.com>
2025-03-25 18:36:34 +0100

    feat: added unique validation for identifier, changed logic for initials

--- src/main/java/acme/constraints/ManagerValidator.java
+++ src/main/java/acme/constraints/ManagerValidator.java
@@ -3,16 +3,23 @@ package acme.constraints;
 
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import acme.client.components.principals.DefaultUserIdentity;
 import acme.client.components.validation.AbstractValidator;
 import acme.client.helpers.StringHelper;
-import acme.realms.Manager;
+import acme.realms.manager.Manager;
+import acme.realms.manager.ManagerRepository;
 
 public class ManagerValidator extends AbstractValidator<ValidManager, Manager> {
 
 	// Internal state ---------------------------------------------
 
+	@Autowired
+	private ManagerRepository repository;
+
 	// ConstraintValidator interface ------------------------------
+
 
 	@Override
 	protected void initialise(final ValidManager annotation) {
@@ -31,17 +38,24 @@ public class ManagerValidator extends AbstractValidator<ValidManager, Manager> {
 		else if (StringHelper.isBlank(manager.getIdentifier()))
 			super.state(context, false, "identifier", "javax.validation.constraints.NotBlank.message");
 		else {
+
+			{
+				boolean uniqueManager;
+				Manager existingManager;
+
+				existingManager = this.repository.findManagerByIdentifier(manager.getIdentifier());
+				uniqueManager = existingManager == null || existingManager.equals(manager);
+
+				super.state(context, uniqueManager, "ticker", "acme.validation.manager.duplicated-identifier.message");
+			}
+
 			boolean containsInitials;
 			DefaultUserIdentity identity = manager.getIdentity();
-			String[] nombreCompleto = (identity.getName() + " " + identity.getSurname()).trim().split("\\s+");
-			StringBuilder initials = new StringBuilder();
-
-			for (String palabra : nombreCompleto)
-				if (!palabra.isEmpty())
-					initials.append(palabra.charAt(0));
-
-			containsInitials = StringHelper.startsWith(manager.getIdentifier(), initials.toString(), false);
-			super.state(context, containsInitials, "identifier", "acme.validation.customer.identifier.message");
+			char nameFirstLetter = identity.getName().charAt(0);
+			char surnameFirstLetter = identity.getSurname().charAt(0);
+			String initials = "" + nameFirstLetter + surnameFirstLetter;
+			containsInitials = StringHelper.startsWith(manager.getIdentifier(), initials, false);
+			super.state(context, containsInitials, "identifier", "acme.validation.manager.identifier.message");
 		}
 
 		result = !super.hasErrors(context);
