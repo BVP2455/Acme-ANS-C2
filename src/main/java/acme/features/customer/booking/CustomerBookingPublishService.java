4647a3b065e49a247a6869571045defa4ebd17df
Miguel3114 <my.garciaazuara@gmail.com>
2025-03-30 11:01:47 +0200

    feat(customer bookings): primera version de la feature de bookings de customer

---
+++ src/main/java/acme/features/customer/booking/CustomerBookingPublishService.java
@@ -0,0 +1,82 @@
+
+package acme.features.customer.booking;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
+import acme.client.helpers.MomentHelper;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.booking.Booking;
+import acme.entities.booking.TravelClass;
+import acme.realms.customer.Customer;
+
+@GuiService
+public class CustomerBookingPublishService extends AbstractGuiService<Customer, Booking> {
+
+	@Autowired
+	private CustomerBookingRepository repository;
+
+
+	@Override
+	public void authorise() {
+		boolean status = super.getRequest().getPrincipal().hasRealmOfType(Customer.class);
+
+		super.getResponse().setAuthorised(status);
+
+		int customerId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		int bookingId = super.getRequest().getData("id", int.class);
+		Booking booking = this.repository.findBookingById(bookingId);
+
+		super.getResponse().setAuthorised(customerId == booking.getCustomer().getId());
+	}
+
+	@Override
+	public void load() {
+
+		int id = super.getRequest().getData("id", int.class);
+		Booking booking = this.repository.findBookingById(id);
+
+		super.getBuffer().addData(booking);
+	}
+
+	@Override
+	public void bind(final Booking booking) {
+		super.bindObject(booking, "flight", "locatorCode", "travelClass", "price", "lastNibble");
+	}
+
+	@Override
+	public void validate(final Booking booking) {
+		if (booking.getLastCardNibble() == null || booking.getLastCardNibble().toString().isBlank())
+			super.state(false, "lastNibble", "acme.validation.lastNibble.message");
+		Booking existing = this.repository.findBookingByLocatorCode(booking.getLocatorCode());
+		boolean valid = existing == null || existing.getId() == booking.getId();
+		super.state(valid, "locatorCode", "customer.booking.form.error.duplicateLocatorCode");
+	}
+
+	@Override
+	public void perform(final Booking booking) {
+		booking.setPurchaseMoment(MomentHelper.getCurrentMoment());
+		this.repository.save(booking);
+	}
+
+	@Override
+	public void unbind(final Booking booking) {
+		Dataset dataset;
+		SelectChoices travelClasses;
+
+		travelClasses = SelectChoices.from(TravelClass.class, booking.getTravelClass());
+
+		Collection<acme.entities.flights.Flight> flights = this.repository.findAllFlights();
+		SelectChoices flightChoices = SelectChoices.from(flights, "id", booking.getFlight());
+
+		dataset = super.unbindObject(booking, "flight", "locatorCode", "travelClass", "price", "lastNibble", "draftMode", "id");
+		dataset.put("travelClasses", travelClasses);
+		dataset.put("flights", flightChoices);
+		super.getResponse().addData(dataset);
+	}
+
+}
