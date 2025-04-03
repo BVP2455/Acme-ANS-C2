567ada1f7dfd3a8700ce16bc90ef602b23a1170c
Francisco Gago <frangago71@gmail.com>
2025-04-03 22:48:18 +0200

    feat: Operations by Administrators on Airports (list,show,update,create)

--- src/main/java/acme/entities/airport/AirportRepository.java
+++ src/main/java/acme/entities/airport/AirportRepository.java
@@ -4,8 +4,10 @@ package acme.entities.airport;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import acme.client.repositories.AbstractRepository;
+
 @Repository
-public interface AirportRepository {
+public interface AirportRepository extends AbstractRepository {
 
 	@Query("select a from Airport a where a.iataCode = :iataCode")
 	Airport findAirportByIataCode(String iataCode);
