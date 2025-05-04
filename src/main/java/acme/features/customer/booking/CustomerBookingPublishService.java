43e640190a398d14c96743b5ac6f4e5e45cef5cd
Miguel3114 <my.garciaazuara@gmail.com>
2025-05-04 13:24:04 +0200

    feat(Booking publish feature): añadida proteccion al post hacking al elegir vuelo invalido o vuelo en draftmode. Además se ha añadido proteccion al get hacking cuando cambio en la url show por publish. Por ultimo se ha añadido validacion para que no deje publica una booking si sus pasajeros no están publicados

--- src/main/java/acme/features/customer/booking/CustomerBookingPublishService.java
+++ src/main/java/acme/features/customer/booking/CustomerBookingPublishService.java
@@ -2,6 +2,7 @@
 package acme.features.customer.booking;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -25,14 +26,34 @@ public class CustomerBookingPublishService extends AbstractGuiService<Customer, 
 	@Override
 	public void authorise() {
 		boolean status = super.getRequest().getPrincipal().hasRealmOfType(Customer.class);
-
 		super.getResponse().setAuthorised(status);
 
-		int customerId = super.getRequest().getPrincipal().getActiveRealm().getId();
-		int bookingId = super.getRequest().getData("id", int.class);
-		Booking booking = this.repository.findBookingById(bookingId);
+		if (status) {
+			int customerId = super.getRequest().getPrincipal().getActiveRealm().getId();
+			int bookingId = super.getRequest().getData("id", int.class);
+			Booking booking = this.repository.findBookingById(bookingId);
+			status = customerId == booking.getCustomer().getId();
+			super.getResponse().setAuthorised(status);
+		}
 
-		super.getResponse().setAuthorised(customerId == booking.getCustomer().getId());
+		if (status && super.getRequest().getMethod().equals("POST")) {
+
+			Integer flightId = super.getRequest().getData("flight", Integer.class);
+			Flight flight = super.getRequest().getData("flight", Flight.class);
+
+			Collection<Flight> flights = this.repository.findAllFlights().stream().filter(f -> f.getNumberLegs() != 0).collect(Collectors.toList());
+			Collection<Flight> flightsAvaiables = flights.stream().filter(f -> f.getScheduledDeparture().after(MomentHelper.getCurrentMoment()) && !f.getDraftMode()).collect(Collectors.toList());
+
+			if (flightId != 0 && !flightsAvaiables.contains(flight))
+				status = false;
+
+			if (flight != null && flight.getDraftMode())
+				status = false;
+
+			super.getResponse().setAuthorised(status);
+
+		}
+
 	}
 
 	@Override
@@ -46,7 +67,7 @@ public class CustomerBookingPublishService extends AbstractGuiService<Customer, 
 
 	@Override
 	public void bind(final Booking booking) {
-		super.bindObject(booking, "flight", "locatorCode", "travelClass", "price", "lastCardNibble");
+		super.bindObject(booking, "flight", "locatorCode", "travelClass", "lastCardNibble");
 	}
 
 	@Override
@@ -59,6 +80,9 @@ public class CustomerBookingPublishService extends AbstractGuiService<Customer, 
 
 		boolean emptyPassengers = !this.repository.findPassengersByBookingId(booking.getId()).isEmpty();
 		super.state(emptyPassengers, "price", "customer.booking.form.error.emptyPassengers");
+
+		boolean publishedPassengers = this.repository.findPassengersByBookingId(booking.getId()).stream().allMatch(p -> p.getDraftMode() == false);
+		super.state(publishedPassengers, "price", "customer.booking.form.error.unpublishedPassengers");
 
 		boolean laterFlight = MomentHelper.isAfter(booking.getFlight().getScheduledDeparture(), MomentHelper.getCurrentMoment());
 		super.state(laterFlight, "flight", "customer.booking.form.error.flightBeforeBooking");
