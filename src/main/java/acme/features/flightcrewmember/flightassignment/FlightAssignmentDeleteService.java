0244a70ad3d4fcd7bbb40a34ab36953a47650a6d
cgaleam <cargalmag@alum.us.es>
2025-05-23 22:12:47 +0200

    fix: fix delete bug in url hacking

--- src/main/java/acme/features/flightcrewmember/flightassignment/FlightAssignmentDeleteService.java
+++ src/main/java/acme/features/flightcrewmember/flightassignment/FlightAssignmentDeleteService.java
@@ -95,7 +95,7 @@ public class FlightAssignmentDeleteService extends AbstractGuiService<FlightCrew
 		flightCrewMembers = this.repository.findAllFlightCrewMembers();
 		flightCrewMemberChoice = SelectChoices.from(flightCrewMembers, "employeeCode", flightAssignment.getFlightCrewMember());
 
-		dataset = super.unbindObject(flightAssignment, "duty", "lastUpdateMoment", "currentStatus", "remarks", "leg", "flightCrewMember");
+		dataset = super.unbindObject(flightAssignment, "duty", "lastUpdateMoment", "currentStatus", "remarks", "leg", "flightCrewMember", "draftMode");
 		dataset.put("dutyChoice", dutyChoice);
 		dataset.put("currentStatusChoice", currentStatusChoice);
 		dataset.put("legChoice", legChoice);
